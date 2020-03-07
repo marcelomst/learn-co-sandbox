cb720ee06b158010e5ca0e29342f34be2c52cec9
Marcelo Martinez <marcelomst1@gmail.com>
2020-03-07 18:48:07 +0000

    Automatically backed up by Learn

---
+++ person.rb
@@ -0,0 +1,16 @@
+class Person
+ 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+end
